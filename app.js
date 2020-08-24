d54feb64a87e04691fe324de9d512a49de568618
Himangshu Mondal <mailtoherock@gmail.com>
2020-08-24 23:34:52 +0530

    added circle on canvas

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ ctx.fillRect(25, 25, 100, 100);
 ctx.fillStyle = "coral";
 ctx.fillRect(135, 25, 100, 100);
 
+// circle
+ctx.fillStyle = "tomato";
+ctx.beginPath();
+ctx.arc(300,76,50,0,2 * Math.PI,false);
+ctx.fill();
+
+ctx.fillStyle = "coral";
+ctx.beginPath();
+ctx.arc(410,76,50,0,2 * Math.PI,false);
+ctx.fill();
