fd65d3cd15670d454bad5ce0c0c2aeef034d15f9
Himangshu Mondal <mailtoherock@gmail.com>
2020-08-25 22:52:40 +0530

    added radialgradient and text

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext('2d');
 
-let gradient = ctx.createLinearGradient(50,0,150,120);
+let linearGradient = ctx.createLinearGradient(50,0,150,120);
+linearGradient.addColorStop(0,"tomato");
+linearGradient.addColorStop(1,"blue");
 
-gradient.addColorStop(0,"tomato");
-gradient.addColorStop(1,"blue");
+let radialGradient = ctx.createRadialGradient(300, 76, 5, 300, 76, 80);
+radialGradient.addColorStop(0,"tomato");
+radialGradient.addColorStop(1,"blue");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // rectangle
-ctx.fillStyle = gradient;
+ctx.fillStyle = linearGradient;
 ctx.fillRect(25, 25, 100, 100);
 
 ctx.fillStyle = "coral";
 ctx.fillRect(135, 25, 100, 100);
 
 // circle
-ctx.fillStyle = "tomato";
+ctx.fillStyle = radialGradient;
 ctx.beginPath();
 ctx.arc(300,76,50,0,2 * Math.PI,false);
 ctx.fill();
@@ -26,3 +29,10 @@ ctx.fillStyle = "coral";
 ctx.beginPath();
 ctx.arc(410,76,50,0,2 * Math.PI,false);
 ctx.fill();
+
+
+// text
+
+ctx.font = "24px sans-serif";
+ctx.fillStyle = "green";
+ctx.fillText("Hello There!", 500, 75);
