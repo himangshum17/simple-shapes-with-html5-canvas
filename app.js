c62dbb5156a836863ae639042fe6a08b1cecc127
Himangshu Mondal <mailtoherock@gmail.com>
2020-08-26 22:39:01 +0530

    added bounce effect

--- app.js
+++ app.js
@@ -7,15 +7,29 @@ canvas.height = window.innerHeight;
 // rectangle
 
 let x = 200;
+let y = 200;
+let dx = 5;
+let dy = 5;
+let width = 100;
+let height = 100;
 
 function animation(){
     requestAnimationFrame(animation);
     ctx.clearRect(0,0,innerWidth,innerHeight);
 
     ctx.fillStyle = "tomato";
-    ctx.fillRect(x, 200, 100, 100);
+    ctx.fillRect(x, y, width, height);
+    
+    if (x + width > innerWidth || x < 0) {
+       dx = -dx;
+    }
 
-    x += 1;
+    if (y + height > innerHeight || y < 0) {
+        dy = -dy;
+     }
+
+    x += dx;
+    y += dy;
 }
 
 animation();
